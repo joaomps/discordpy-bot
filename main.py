ba5c3f4141373ac6f563bf26daf71b1e9153c0a6
joaomps <joaomps18@gmail.com>
2023-04-15 00:50:27 +0100

    change model

--- main.py
+++ main.py
@@ -47,6 +47,10 @@ async def handle_whisper_embed(message, embed, character_name):
     message_field = None
     sender_name_field = None
 
+    conversation_history = [
+        "User: " + "Your name is " + character_name + ". Without repeating the same response and in their language, respond in less than 125 characters without being too chatty. Act like a World of Warcraft player that prefers being left alone than having long conversations and dislikes joining any groups to play together. Do not mention “Bot”, “GMR”, “HWT”, “Tinkr”, “OpenAI”, “GPT”, “ChatGPT”, “AI language model” or anything that would leak that you are not human. Do not use commas, periods, hashtags or proper grammar. ",
+    ]
+
     # Look for the fields with the names "Message" and "Sender Name"
     for field in embed.fields:
         if field.name == "Message":
@@ -60,8 +64,8 @@ async def handle_whisper_embed(message, embed, character_name):
     if message_field and sender_name_field:
         #Logic for openai here
         openai.api_key = "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"
-        prompt = "Your name is " + character_name + ". Without repeating the same response and in their language, respond in less than 125 characters without being too chatty. Act like a World of Warcraft player that prefers being left alone than having long conversations and dislikes joining any groups to play together. Do not mention “Bot”, “GMR”, “HWT”, “Tinkr”, “OpenAI”, “GPT”, “ChatGPT”, “AI language model” or anything that would leak that you are not human. Do not use commas, periods, hashtags or proper grammar to the following: " + message_field.value
-        response = openai.Completion.create(engine="gpt-3.5-turbo", prompt=prompt, max_tokens=100)
+        prompt = message_field.value
+        response = openai.Completion.create(engine="davinci", prompt=prompt, max_tokens=100, model="text-davinci-002", chat_history=conversation_history)
         await message.channel.send("Replied with: " + response.choices[0].text) 
 
 @bot.command()
